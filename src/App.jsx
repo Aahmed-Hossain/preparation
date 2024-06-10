e49eb81852044986f314ca393000cc634722a3db
Aahmed-Hossain <aahmdctec@gmail.com>
2024-06-10 20:21:13 +0600

    disable a button practiced

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react"
 
 
 function App() {
-
+const [value, setValue] = useState('')
   return (
     <div>
-      
+      <h1>disable the button</h1>
+<input type="text" placeholder="type 'delete' to activate"
+ value={value} onChange={(e)=>setValue(e.target.value)}/>
+<button disabled ={value  !== 'delete'}>
+  click
+</button>
     </div>
   )
 }
